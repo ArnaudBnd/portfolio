7a11018f184403497f87d8a9cc3050f411398614
arnaudbnd <arnaud.benede.contact@gmail.com>
2025-02-14 17:07:31 +0100

    feat: update CV url

--- components/ResumeBtn.tsx
+++ components/ResumeBtn.tsx
@@ -69,7 +69,7 @@ export default function ResumeBtn() {
   return (
     <a
       role="button"
-      href="https://drive.google.com/file/d/18MyJh5cPYgN1h8yHt25pXb0h_RirrTdZ/view?usp=drive_link"
+      href="https://drive.google.com/file/d/1kZr3jv4svHZNDLroWKkCrd79Q6HC9pwy/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Cv"
